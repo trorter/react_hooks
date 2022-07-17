62d21e6f7aa864ebdc1c3ac6f7c1cf304301f85e
Andrei Ledovskikh <auledovskih@gmail.com>
2022-07-17 13:05:05 +0400

    useState hook

---
+++ src/useState/HookCounter.tsx
@@ -0,0 +1,44 @@
+import React, {useState} from "react"
+
+const HookCounter = () => {
+  const [name, setName] = useState({firstName: '', lastName: ''})
+
+  return (
+    <div>
+      <form>
+        <div style={{
+          marginBottom: "5px"
+        }}>
+          <label>
+            <span style={{
+              marginRight: "5px"
+            }}>
+              First name:</span>
+            <input type={"text"} onChange={(event) => setName({...name, firstName: event.target.value})}/>
+          </label>
+        </div>
+
+        <div style={{
+          marginBottom: "5px"
+        }}>
+          <label>
+            <span style={{
+              marginRight: "5px"
+            }}>
+              Last name:
+            </span>
+            <input type={"text"} onChange={(event) => setName({...name, lastName: event.target.value})}/>
+          </label>
+        </div>
+
+        <div>
+          <h2>Your first name is - {name.firstName}</h2>
+          <h2>Your last name is - {name.lastName}</h2>
+        </div>
+
+      </form>
+    </div>
+  )
+}
+
+export default HookCounter
